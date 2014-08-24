1ac58da29d340eb902b91f08f25b1bebf110e96b
Brian Knox <bknox@digitalocean.com>
2014-08-24 12:27:45 -0400

    noise service

--- cmd/example_revisit_service/main.go
+++ cmd/example_revisit_service/main.go
@@ -5,28 +5,42 @@ import (
 	"image"
 	"image/color"
 	"log"
+	"math/rand"
 )
 
-type ImageSet interface {
-	Set(x, y int, c color.Color)
-}
-
-// rotate stolen from Justin Abrahms while I make sure my interface works
-// and is compatible!  See - https://github.com/justinabrahms/rotate.revisit.link
-func rotate(src image.Image, dst image.RGBA) error {
+func noise(src image.Image, dst image.RGBA) error {
 	orig := src.Bounds()
+
 	for x := orig.Min.X; x < orig.Max.X; x++ {
 		for y := orig.Min.Y; y < orig.Max.Y; y++ {
-			dst.Set(y,
-				orig.Max.Y-x+1,
-				src.At(x, y))
+			dst.Set(x, y, src.At(x, y))
 		}
 	}
+
+	numToMod := (orig.Max.X * orig.Max.Y) / 2
+	for i := 0; i < numToMod; i++ {
+		x := rand.Intn(orig.Max.X)
+		y := rand.Intn(orig.Max.Y)
+		if prev, ok := src.At(x, y).(color.RGBA); ok {
+			prev.R += 30
+			prev.B -= 30
+			prev.G += 30
+			dst.Set(x, y, prev)
+		}
+		if prev, ok := src.At(x, y).(color.YCbCr); ok {
+			prev.Cr += 30
+			prev.Cb -= 30
+			prev.Y += 30
+			dst.Set(x, y, prev)
+		}
+	}
+
 	return nil
+
 }
 
 func main() {
 	log.Println("starting")
-	s := revisit.NewRevisitService(rotate)
+	s := revisit.NewRevisitService(noise)
 	s.Run()
 }
