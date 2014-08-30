6738c7f944cff577ce9d29635e441dc2785e0142
Brian Knox <bknox@digitalocean.com>
2014-08-30 13:07:40 -0400

    working on image revisitor tests

--- image_revisitor_test.go
+++ image_revisitor_test.go
@@ -34,3 +34,63 @@ func TestImageRevisitorJPEG(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestImageRevisitorPNG(t *testing.T) {
+	pngTestFunc := func(src image.Image, dst image.RGBA) error {
+		orig := src.Bounds()
+		for x := orig.Min.X; x < orig.Max.X; x++ {
+			for y := orig.Min.Y; y < orig.Max.Y; y++ {
+				dst.Set(x, y, src.At(x, y))
+			}
+		}
+		return nil
+	}
+
+	pngMsg, err := NewRevisitMsgFromFiles("./fixtures/connie.png")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if pngMsg.ImageType() != "image/png" {
+		t.Error(err)
+	}
+
+	msg, err := ImageRevisitor(pngMsg, pngTestFunc)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if msg.ImageType() != "image/png" {
+		t.Error(err)
+	}
+}
+
+func TestImageRevisitorGIF(t *testing.T) {
+	gifTestFunc := func(src image.Image, dst image.RGBA) error {
+		orig := src.Bounds()
+		for x := orig.Min.X; x < orig.Max.X; x++ {
+			for y := orig.Min.Y; y < orig.Max.Y; y++ {
+				dst.Set(x, y, src.At(x, y))
+			}
+		}
+		return nil
+	}
+
+	gifMsg, err := NewRevisitMsgFromFiles("./fixtures/bob.gif")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if gifMsg.ImageType() != "image/gif" {
+		t.Error(err)
+	}
+
+	msg, err := ImageRevisitor(gifMsg, gifTestFunc)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if msg.ImageType() != "image/gif" {
+		t.Error(err)
+	}
+}
