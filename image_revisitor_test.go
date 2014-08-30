0e56a2509743734c17d71fd828b86f33cb227238
Brian Knox <bknox@digitalocean.com>
2014-08-30 13:04:43 -0400

    working on image revisitor tests

--- image_revisitor_test.go
+++ image_revisitor_test.go
@@ -5,15 +5,32 @@ import (
 	"testing"
 )
 
-func testFunc(src image.Image, dst image.RGBA) error {
-	orig := src.Bounds()
-	for x := orig.Min.X; x < orig.Max.X; x++ {
-		for y := orig.Min.Y; y < orig.Max.Y; y++ {
-			dst.Set(x, y, src.At(x, y))
-		}
-	}
-	return nil
-}
-
 func TestImageRevisitorJPEG(t *testing.T) {
+	jpegTestFunc := func(src image.Image, dst image.RGBA) error {
+		orig := src.Bounds()
+		for x := orig.Min.X; x < orig.Max.X; x++ {
+			for y := orig.Min.Y; y < orig.Max.Y; y++ {
+				dst.Set(x, y, src.At(x, y))
+			}
+		}
+		return nil
+	}
+
+	jpgMsg, err := NewRevisitMsgFromFiles("./fixtures/bob.jpg")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if jpgMsg.ImageType() != "image/jpeg" {
+		t.Error(err)
+	}
+
+	msg, err := ImageRevisitor(jpgMsg, jpegTestFunc)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if msg.ImageType() != "image/jpeg" {
+		t.Error(err)
+	}
 }
